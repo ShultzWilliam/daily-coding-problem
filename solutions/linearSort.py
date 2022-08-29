240cdc9b17f7e66a7b2e2863a062d629d6d240bb
shultzWilliamLinux <william.shultz2017@gmail.com>
2022-08-28 22:12:47 -0400

    added simple linear sort: ADD ADVANCED VERSION LATER

---
+++ solutions/linearSort.py
@@ -0,0 +1,25 @@
+#Given an unsorted list, implement the linear sort algorithm to sort the list in ascending order
+#INPUT: in: list[int]
+#OUTPUT: list[int]
+
+def swap(a,b):
+    temp = a
+    a = b
+    b = temp
+
+def sort(x):
+    returnable = []
+    for i in range(len(x)):
+        lowest = x[i]
+        for j in range(i,len(x)):
+            if x[j] <= lowest:
+                lowest = x[j]
+        returnable.append(lowest)
+    return returnable
+
+def test():
+    a = [4,7,1,6,9,5,2,6]
+    sort(a)
+
+
+    print(sort(a))
